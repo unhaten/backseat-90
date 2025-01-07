70bba6132e3450ee298a2551cfc3184c51c680fe
Max Barren <csgold2002@mail.ru>
2025-01-07 21:22:21 +0300

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,33 @@
 import { Player } from '@/widgets/player'
-import { Song } from '@/widgets/song'
 import { Turntable } from '@/widgets/turntable'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home() {
 	return (
 		<>
 			{/* //* Background GIF #93c5fd */}
 			{/* FIXME: //! if it is webp then we need to set unoptimized property */}
-			<Image
+			{/* <Image
 				className='absolute top-0 left-0 z-0 w-full h-full object-center'
 				src={'/gif3.gif'}
 				alt='the gif'
 				unoptimized
 				fill
+			/> */}
+			<Image
+				className='absolute top-0 left-0 z-0 w-full h-full object-center'
+				src={'/pattern.svg'}
+				alt='the image'
+				fill
 			/>
 			<div className='relative flex items-center bg-background/30 justify-center min-h-screen w-screen sm:p-8'>
 				{/* //* Content */}
-				<div className='bg-blue-300/35 rounded-lg p-8'>
-					<div className='relative flex flex-col items-center justify-center h-screen sm:h-full'>
-						<h1 className='font-rockSalt text-3xl sm:text-6xl my-0'>
+				<div className='bg-rose-500/75 rounded-lg p-4 md:p-8 w-96'>
+					{/* <h1 className='font-rockSalt text-3xl sm:text-6xl my-0'>
 							Backseat 90
-						</h1>
-						<Turntable />
-						<div className='flex items-center gap-2 w-full'>
-							<Song />
-							<Player />
-						</div>
-					</div>
+						</h1> */}
+					<Turntable />
+					<Player />
 				</div>
 			</div>
 		</>
--- src/widgets/player/ui.tsx
+++ src/widgets/player/ui.tsx
@@ -3,6 +3,7 @@
 import { useAppSelector } from '@/lib/hooks/redux'
 import { useEffect, useRef } from 'react'
 import { Controls } from './components'
+import Image from 'next/image'
 
 export const Player = () => {
 	const ref = useRef<HTMLAudioElement>(null)
@@ -21,6 +22,19 @@ export const Player = () => {
 	return (
 		<div>
 			<audio ref={ref} src='/hiphopproject-rare.mp3' />
+			<div>
+				<div className='flex align-center gap-4'>
+					<div className='relative w-24 h-24'>
+						<Image
+							src='/song-placeholder.png'
+							fill
+							alt='song placeholder'
+						/>
+					</div>
+					<h3>Now playing:</h3>
+					<h4>Song name</h4>
+				</div>
+			</div>
 			<Controls />
 		</div>
 	)
