660066c2506b5005b0246a0f2c889d672a849784
Max Barren <csgold2002@mail.ru>
2025-01-10 17:42:02 +0300

    up of non existing lib

--- src/widgets/turntable/components/vinyl/components/index.ts
+++ src/widgets/turntable/components/vinyl/components/index.ts
@@ -1,5 +1,4 @@
 export { NeedleContainer } from './needle-container'
-export { Label } from './label'
 export { Line } from './line'
 export { Needle } from './needle'
 export { NeedleConstruction } from './needle-construction'
--- src/widgets/turntable/components/vinyl/components/label.tsx
+++ src/widgets/turntable/components/vinyl/components/label.tsx
@@ -1,10 +1,10 @@
 import { motion } from 'motion/react'
 import s from '../vinyl.module.css'
-import { useMediaQuery } from '@siberiacancode/reactuse'
+// import { useMediaQuery } from '@siberiacancode/reactuse'
 
 type Props = { color: string; isPlaying: boolean }
 export const Label = ({ color, isPlaying }: Props) => {
-	const isMobile = useMediaQuery('(max-width: 640px)')
+	// const isMobile = useMediaQuery('(max-width: 640px)')
 
 	const motionProps = isPlaying
 		? {
@@ -23,17 +23,17 @@ export const Label = ({ color, isPlaying }: Props) => {
 		<motion.div
 			{...motionProps}
 			className={s.label}
-			style={
-				isMobile
-					? {
-							boxShadow: `0 0 0 4px ${color}, inset 0 0 0 11px ${color}`,
-							border: `solid 1.25px #d9a388`
-					  }
-					: {
-							boxShadow: `0 0 0 4px ${color}, inset 0 0 0 27px ${color}`,
-							border: `solid 1.25px #d9a388`
-					  }
-			}
+			// style={
+			// 	isMobile
+			// 		? {
+			// 				boxShadow: `0 0 0 4px ${color}, inset 0 0 0 11px ${color}`,
+			// 				border: `solid 1.25px #d9a388`
+			// 		  }
+			// 		: {
+			// 				boxShadow: `0 0 0 4px ${color}, inset 0 0 0 27px ${color}`,
+			// 				border: `solid 1.25px #d9a388`
+			// 		  }
+			// }
 			initial={{ scale: 1 }}
 		/>
 	)
