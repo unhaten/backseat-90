a0d0b604e2c6286a4fadb6349c7379ab6d6c8fea
Max Barren <csgold2002@mail.ru>
2025-02-03 12:51:21 +0300

    image fetching sound + disabling button while isFetching

--- src/widgets/background-image/components/change-image/ui.tsx
+++ src/widgets/background-image/components/change-image/ui.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { Button } from '@/components/ui'
-import { useQueryClient } from '@tanstack/react-query'
+import { useIsFetching, useQueryClient } from '@tanstack/react-query'
 import { ImagePlay } from 'lucide-react'
 
 export const ChangeImage = () => {
 	// TODO: make an animation between image changes
 	const queryClient = useQueryClient()
+	const isFetching = useIsFetching({ queryKey: ['image-generator'] }) > 0
 
 	const handleClick = () => {
+		new Audio('/noise.mp3').play()
 		queryClient.invalidateQueries({
 			queryKey: ['image-generator']
 		})
@@ -20,6 +22,7 @@ export const ChangeImage = () => {
 			variant={'outline'}
 			size='icon'
 			onClick={handleClick}
+			disabled={isFetching}
 		>
 			<ImagePlay />
 		</Button>
